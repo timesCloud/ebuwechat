dcaff60ea5ecd9d5999f00c0628dce250f54bfaa
elorita <delicate.tsao@live.cn>
2015-10-30 21:06:41 +0800

    首次上传

---
+++ js/leanCloudChange.js
@@ -0,0 +1,5 @@
+var  formalID='<script>AV.initialize("efqmjf2gco3z0b9eqlvx714kfvzfpkx94mrr4xbbi7pzu4ts", "9xu5smmtkpnvz6h3k8r82dn6kfims9d8hpg2je5tztmot4wf");</script>';
+var testID='<script>AV.initialize("56fN6rijVJvWJo6EMTwcoMLs", "Fl3vxu3ssxOHmnJARKqELp0m");</script>';
+window.onload=function(){
+
+};
